4418581e8290a676bc7b9036bbd944a77f89b331
diegotalejandro <diego.torreblanca2013@gmail.com>
2020-10-18 16:33:11 -0300

    Second Day

--- client/client.js
+++ client/client.js
@@ -1,8 +1,13 @@
+//const { response } = require("express");
+
 const form = document.querySelector("form");
 const loadingElement = document.querySelector(".loading");
-const API_URL = = 'http://localhost:5000/new';
+const newsElement = document.querySelector(".news");
+const API_URL = "http://localhost:5000/news";
 
-loadingElement.style.display = "none";
+loadingElement.style.display = "";
+
+listAllNews();
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -18,10 +23,47 @@ form.addEventListener("submit", (event) => {
   loadingElement.style.display = "";
 
   fetch(API_URL, {
-      method: 'POST',
-      body: JSON.stringify(jsonDataToSend),
-      headers: {
-          'content-type': 'application/json' 
-      }
+    method: "POST",
+    body: JSON.stringify(jsonDataToSend),
+    headers: {
+      "content-type": "application/json",
+    },
   })
+    .then((response) => response.json())
+    .then((createdNew) => {
+      form.reset();
+      setTimeout(() => {
+        form.style.display = "";
+      }, 30000);
+
+      listAllNews();
+      loadingElement.style.display = "none";
+    });
 });
+
+function listAllNews() {
+  newsElement.innerHTML = "";
+  fetch(API_URL)
+    .then((response) => response.json())
+    .then((news) => {
+      news.reverse();
+      news.forEach((element) => {
+        const div = document.createElement("div");
+        const header = document.createElement("h3");
+        header.textContent = element.name;
+
+        const contents = document.createElement("p");
+        contents.textContent = element.content;
+
+        const date = document.createElement("small");
+        date.textContent = new Date(element.created_date);
+
+        div.appendChild(header);
+        div.appendChild(contents);
+        div.appendChild(date);
+
+        newsElement.appendChild(div);
+      });
+    });
+  loadingElement.style.display = "none";
+}
--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const cors = require("cors");
+const monk = require("monk");
+const Filter = require("bad-words");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
+
+const db = monk("localhost:27017/twitter-clone");
+const news = db.get("news");
+
+const filter = new Filter();
+
+app.use(cors()); //
+app.use(express.json()); //JSON Parse Middleware
 
 app.get("/", (request, response) => {
   response.json({
@@ -8,8 +20,46 @@ app.get("/", (request, response) => {
   });
 });
 
-app.post("/new", (req, res) => {
-  console.log(req.body);
+app.get("/news", (req, res) => {
+  news.find().then((newsJsonData) => {
+    res.json(newsJsonData);
+  });
+});
+
+function isValidNew(newJsonData) {
+  return (
+    newJsonData.name &&
+    newJsonData.name.toString().trim() !== "" &&
+    newJsonData.content &&
+    newJsonData.content.toString().trim() !== ""
+  );
+}
+
+app.use(
+  rateLimit({
+    windowMs: 30 * 1000, // 30 seconds
+    max: 1, // limit each IP to 1 requests per windowMs
+  })
+);
+
+app.post("/news", (req, res) => {
+  if (isValidNew(req.body)) {
+    //insert into db
+    const newJsonData = {
+      name: filter.clean(req.body.name.toString()),
+      content: filter.clean(req.body.content.toString()),
+      created_date: new Date(),
+    };
+
+    news.insert(newJsonData).then((createdNew) => {
+      res.json(createdNew);
+    });
+  } else {
+    res.status(422);
+    res.json({
+      message: "Name and Content are required!",
+    });
+  }
 });
 
 app.listen(5000, () => {
